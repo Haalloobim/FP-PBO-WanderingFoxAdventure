9cb52677a98d4d6504b4759238812aaa5da31829
Haalloobim <withbimatara@gmail.com>
2023-11-27 22:55:31 +0700

    feat: add constructor to tile.java

--- src/id/ac/its/fox/tilemap/Tile.java
+++ src/id/ac/its/fox/tilemap/Tile.java
@@ -6,5 +6,13 @@ public class Tile {
     private BufferedImage image;
     private int type; 
 
+    public static final int NORMAL = 0;
+	public static final int BLOCKED = 1;
+
+    public Tile(BufferedImage image, int type) {
+		this.image = image;
+		this.type = type;
+	}
+
     
 }
