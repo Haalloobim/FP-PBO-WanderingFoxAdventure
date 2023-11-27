ea20e2a0c505c03b785afbc4364a48a17794a1ed
Haalloobim <withbimatara@gmail.com>
2023-11-27 22:44:58 +0700

    feat: completing Audio Class

--- src/id/ac/its/fox/audio/AudioPlayer.java
+++ src/id/ac/its/fox/audio/AudioPlayer.java
@@ -4,7 +4,10 @@ import javax.sound.sampled.*;
 
 public class AudioPlayer {
     private Clip clip;
+	private float previousVolume = 0;
+	private float currentVolume = 0;
 	private FloatControl fc;
+	private boolean mute = false;
 
     public AudioPlayer(String path){
         try {
@@ -30,4 +33,61 @@ public class AudioPlayer {
             exception.printStackTrace();
         }
     }
+
+    public void clipPlay(){
+        if(clip == null) return;
+        clipStop();
+        clip.setFramePosition(0);
+        while(!clip.isRunning()){
+            clip.start();
+        }
+    }
+
+    public void clipStop(){
+        if(clip.isRunning()) clip.stop();
+    }
+
+    public void bgplay() {
+		clipPlay();
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+
+    public void clipClose(){
+		clipStop();
+		clip.close();
+	}
+
+    public void volumeUp() {
+		if(mute)
+			mute = false;
+		currentVolume += 5.0f;
+		if(currentVolume > 6.0f)
+			currentVolume = 6.0f;
+		fc.setValue(currentVolume);
+	}
+	
+	public void volumeDown() {
+		if(mute)
+			mute = false;
+		currentVolume -= 5.0f;
+		if(currentVolume < -50.0f) {
+			currentVolume = -50.0f;
+			mute = true;
+		}
+		fc.setValue(currentVolume);
+	}
+	
+	public void volumeMute() {
+		if(!mute) {
+			previousVolume = currentVolume;
+			currentVolume = -50.0f;
+			fc.setValue(currentVolume);
+			mute = true;
+		}
+		else if(mute) {
+			currentVolume = previousVolume;
+			fc.setValue(currentVolume);
+			mute = false;
+		}
+	}
 }
