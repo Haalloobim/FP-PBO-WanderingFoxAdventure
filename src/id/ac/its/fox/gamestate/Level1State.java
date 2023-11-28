729f2882b444a70e7dd3c14535a959d597f5c5f9
Haalloobim <withbimatara@gmail.com>
2023-11-28 16:53:58 +0700

    feat: add background in level1 statw

--- src/id/ac/its/fox/gamestate/Level1State.java
+++ src/id/ac/its/fox/gamestate/Level1State.java
@@ -1,17 +1,25 @@
 package id.ac.its.fox.gamestate;
 
 import id.ac.its.fox.main.GamePanel;
+import id.ac.its.fox.tilemap.Background;
 import id.ac.its.fox.tilemap.TileMap;
 
-import java.awt.Graphics2D;
 import java.awt.*; 
 
 public class Level1State extends GameState{
 
+    private Background bgLevel1;
     private TileMap tilemap; 
 
     public Level1State(GameStateManager gsm) {
         this.gsm = gsm;
+        try {
+            bgLevel1 = new Background("/Background/back.png", 3);
+            bgLevel1.setVector(0, 0);
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -35,7 +43,7 @@ public class Level1State extends GameState{
         g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
 
         tilemap.draw(g);
-
+        bgLevel1.draw(g);
     }
 
     @Override
