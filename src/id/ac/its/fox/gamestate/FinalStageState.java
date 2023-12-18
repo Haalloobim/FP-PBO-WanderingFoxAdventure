44a6e56ae2200806038b3d35eae05bf4049fe4e7
Haalloobim <withbimatara@gmail.com>
2023-12-18 11:29:11 +0700

    init: initiate final stage

---
+++ src/id/ac/its/fox/gamestate/FinalStageState.java
@@ -0,0 +1,58 @@
+package id.ac.its.fox.gamestate;
+
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+
+public class FinalStageState extends GameState{
+
+    @Override
+    public void init() {
+    
+    }
+
+    @Override
+    public void update() {
+        
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        
+    }
+
+    @Override
+    public void keyPressed(int k) {
+        
+    }
+
+    @Override
+    public void keyReleased(int k) {
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent k) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent k) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent k) {
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent k) {
+        
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent k) {
+        
+    }
+    
+}
